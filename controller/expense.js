282d7d5ff411ca32496812af9ea6dea4dd1e3ebc
Nirmal2796 <nirmalgadekar2796@gmail.com>
2024-03-06 17:50:19 +0530

    After refresh show premiumuser

--- controller/expense.js
+++ controller/expense.js
@@ -1,5 +1,17 @@
 const Expense=require('../model/expense');
 
+const User=require('../model/user');
+
+const bcrypt=require('bcrypt');
+
+const jwt=require('jsonwebtoken');
+
+function generateAccessToken(id){ //we will call this funciton when user has successfully logged in.
+    //jst.sign({what you want to encrypt} ,  secret key); never share your secret key and dont even save it to git
+    return jwt.sign({userId: id}, 'secretkey');
+}
+
+
 exports.postExpense=(req,res,next)=>{
 
     amount=req.body.amount;
@@ -27,7 +39,35 @@ exports.getExpenses=(req,res,next)=>{
 
     req.user.getExpenses()
     .then(expenses=>{
-        res.status(200).json({expenses, ispremiumuser: req.user.ispremiumuser});
+        
+        User.findOne({where:{email:email}})
+        .then(user=>{
+             // console.log(user);
+             if(user){
+                 return user;
+             }
+             else{
+                 res.status(404).json('User Not Found');
+             }
+        })
+        .then((user)=>{
+     
+         bcrypt.compare(password,user.password, (err,result)=>{
+             if(err){
+                 res.status(500).json({success:false,message:'Something Went Wrong'});
+             }
+             if(result){
+                res.status(200).json({expenses ,ispremiumuser: user.ispremiumuser ,token:generateAccessToken(user.id)});
+             }
+             else{
+                 res.status(401).json('User not authorized');
+             }
+         })
+     
+         })
+
+
+        
     })
     .catch(err=>{
         console.log(err);
