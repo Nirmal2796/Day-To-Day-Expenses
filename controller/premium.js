f4f2cc9b573917697ab038489d6691dc6fcff18a
Nirmal2796 <nirmalgadekar2796@gmail.com>
2024-03-08 00:41:20 +0530

    Leaderboard Optimisation

--- controller/premium.js
+++ controller/premium.js
@@ -8,12 +8,22 @@ const sequelize = require('../util/database');
 
 exports.showLeaderBoard=(req,res,next)=>{
 
-    Expense.findAll({attributes:[[sequelize.fn('sum',sequelize.col('amount')),'amount']],
-            include:[{model:User, required:false ,right:true}],
-            group:['user.id'],
-            
-        })
+    User.findAll({
+        attributes:['id','uname',[sequelize.fn('sum',sequelize.col('amount')),'amount']],//select 
+        include:[{
+            model:Expense,
+            attributes:[]
+        }],
+        group:['user.id'],
+        order:[[sequelize.col('amount'),'DESC']]
+    })
+
+    // Expense.findAll({attributes:[[sequelize.fn('sum',sequelize.col('amount')),'amount']],
+    //         include:[{model:User, attributes:['id','uname'] ,required:false ,right:true}],
+    //         group:['user.id']
+    //     })
     .then((result)=>{
+        // result.sort((a,b)=> b.amount - a.amount);
         res.status(200).json({result:result});
     })
     .catch(err=>{
