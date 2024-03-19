a30bfb6cef39b3acadf3f6aaecdb56f75d057346
Nirmal2796 <nirmalgadekar2796@gmail.com>
2024-03-19 20:34:38 +0530

    Frontend changes

--- main.js
+++ main.js
@@ -3,17 +3,23 @@ const desc = document.getElementById('description');
 const category = document.getElementById('category');
 const rzp_button=document.getElementById('rzp-button');
 const leaderboard_button= document.getElementById('leaderboard-button');
+const download_button= document.getElementById('download-button');
 
 const form = document.querySelector('form');
 const Eul = document.getElementById('expense-list');
 const ElistDiv=document.getElementById('Elist');
 const LeaderBoardListDiv=document.getElementById('LeaderBoardList');
 const leaderboard_list = document.getElementById('leaderboard-list');
+const DownloadListDiv=document.getElementById('DownloadList');
+const DownloadList = document.getElementById('download-list');
 
+const DownloadH5=document.getElementById('nodownload');
+const ExpensesH5=document.getElementById('noexpenses');
 
 form.addEventListener('submit', onSubmit);
 rzp_button.addEventListener('click',razorpayEvent);
 leaderboard_button.addEventListener('click',showLeaderBoard);
+download_button.addEventListener('click',downloadExpenses);
 
 document.addEventListener('DOMContentLoaded', DomLoad);
 
@@ -39,11 +45,17 @@ async function DomLoad() {
         }
 
         if(res.data.expenses.length > 0){
-            ElistDiv.hidden=false;
+            Eul.hidden=false;
             for (let i in res.data.expenses) {
                 showOnScreen(res.data.expenses[i]);
            }
-        }           
+        }
+        else{
+            ElistDiv.innerHTML=ElistDiv.innerHTML+`<h5 id='noexpenses'>No Expenses</h5>`
+        } 
+        
+        showDownloadedFiles();
+
         }
         catch(err){
         console.log(err)
@@ -73,10 +85,12 @@ async function onSubmit(e) {
                 };
                 let response= await axios.post("http://localhost:3000/add-expense/",expense,{headers:{"Authorization":token}});
                 // console.log(response.data.id);
-                if(ElistDiv.hidden){
-                    ElistDiv.hidden=false;
+                if(Eul.hidden){
+                    Eul.hidden=false;
+                    ExpensesH5.hidden=true;
                 }
                 showOnScreen(response.data);
+                showLeaderBoard();
                 
             }
             catch(err){
@@ -95,6 +109,7 @@ async function removeExpense(id) {
         const token=localStorage.getItem('token');
         await axios.delete(`http://localhost:3000/delete-expense/${id}`,{headers:{"Authorization":token}});
         Eul.removeChild(document.getElementById(id));
+        showLeaderBoard();
                                  
     }
     catch(err){
@@ -107,7 +122,7 @@ async function removeExpense(id) {
 //SHOW ADDED DATA ON SCREEN
 function showOnScreen(obj){
  
-    const child=`<li id=${obj.id}> 
+    const child=`<li id=${obj.id} class="list-group-item"> 
     ${obj.category} - ${obj.amount} - ${obj.description}
     <button class='btn btn-danger btn-sm  mb-2' onClick=removeExpense(${obj.id})>Delete</button> 
     </li>`
@@ -130,26 +145,54 @@ async function showLeaderBoard(){
     
     leaderboard_list.innerHTML='';
 
+
     for(let i in result.data.result){
   
         let child;
 
         if(result.data.result[i].total_expense == null){
-            child=`<li id=${result.data.result[i].id}> 
+            child=`<li id=${result.data.result[i].id} class="list-group-item"> 
                     ${result.data.result[i].uname} - 0 
                     </li>`
         }
         else{
             // console.log(result.data.result[i].user);
-            child=`<li id=${result.data.result[i].id}> 
+            child=`<li id=${result.data.result[i].id} class="list-group-item"> 
                 ${result.data.result[i].uname} - ${result.data.result[i].total_expense}  
                 </li>`
         }
 
             leaderboard_list.innerHTML=leaderboard_list.innerHTML+child;
     }
+
+    LeaderBoardListDiv.scrollIntoView();
     
 }
+
+
+//Download Expenses
+async function downloadExpenses(e){
+
+    try{
+
+        const token=localStorage.getItem('token');
+
+        const result=await axios.get("http://localhost:3000/download",{headers:{"Authorization":token}})
+
+        if(result.status==200){
+             var a=document.createElement('a');
+             a.href=result.data.fileURL;
+             a.download='myexpenses.csv';
+             a.click();
+        }
+
+        showDownloadedFiles();
+    }
+    catch(err){
+
+    }
+}
+
 
 
 
@@ -193,16 +236,61 @@ async function razorpayEvent(e){
 
     //To handle payment failed.
     rzp1.on('payment.failed', async function(response){
-        console.log(response);
+        // console.log(response);            
+        alert('something went wrong');
         await axios.post("http://localhost:3000/purchase/updatetransactionstatus",{
                 order_id:options.order_id,
                 payment_id:response.razorpay_payment_id,
                 status:'Failed'
         },
         {headers:{"Authorization":token}});
-            
-        alert('something went wrong');
     })
 
+
+}
+
+
+
+async function showDownloadedFiles(){
+
+    const token=localStorage.getItem('token');
+
+    const ispremiumuser=localStorage.getItem('ispremiumuser');
+     
+
+    if(ispremiumuser!=null && ispremiumuser=='true'){
+        
+        const downloadList = await axios.get("http://localhost:3000/downloaded-files",{headers:{"Authorization":token}});
+        
+        console.log(downloadList.data.fileUrls);
+
+        if(downloadList.data.fileUrls.length > 0){
+            
+            if(DownloadList.hidden){
+                DownloadList.hidden=false;
+            }
+            
+            for (let i in downloadList.data.fileUrls) {
+                
+                    const child=`<li class="list-group-item"><a href=${downloadList.data.fileUrls[i].url}>
+                                            ${downloadList.data.fileUrls[i].url}</a></li>`
+                                            
+                    DownloadList.innerHTML=DownloadList.innerHTML+child;
+
+           }
+        }
+        else{
+            DownloadListDiv.innerHTML=DownloadListDiv.innerHTML+`<h5 id='nodownload'>No File Downloaded</h5>`
+        }
+    
+    
+    }
+    else{
+        DownloadListDiv.innerHTML=DownloadListDiv.innerHTML+`<h5 id='nodownload'>Not a Premium User</h5>`
+    }
+    
+
+        
+       
 
 }
